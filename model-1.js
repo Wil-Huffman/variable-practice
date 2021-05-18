cd67c09ca63424b161dbf1a2629d893cb227b258
Wil-Huffman <me@wil-huffman.io>
2021-05-18 17:28:30 -0500

    Add Arrays

--- model-1.js
+++ model-1.js
@@ -7,3 +7,6 @@ var timesLetterIIsUsed = 0
 //Boolean
 var isAddedToVariablesPractice = true
 var isHopefullyDoingThisRight = true
+//Arrays
+var favorite-days-of-the-week = [friday, saturday, sunday]
+var turingModules = [ø,1,2,3,4]
