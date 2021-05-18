ba4df5abd75c47e2fa14f6b77d6767a6244acf2e
Wil-Huffman <me@wil-huffman.io>
2021-05-18 17:00:22 -0500

    Add Boolean

--- model-1.js
+++ model-1.js
@@ -4,3 +4,6 @@ var openingPhrase = "Lorem Ipsum"
 //Intergers
 var numOfWords = 142
 var timesLetterIIsUsed = 0
+//Boolean
+var isAddedToVariablesPractice = true
+var isHopefullyDoingThisRight = true
